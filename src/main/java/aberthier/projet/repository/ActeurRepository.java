9081c17a239cb2a2aa6bcebf9d3d39e0f4d23c6e
octogene <ooctogene@gmail.com>
2020-02-07 15:07:20 +0100

    Add search methods to Acteur & Film repositories

--- src/main/java/aberthier/projet/repository/ActeurRepository.java
+++ src/main/java/aberthier/projet/repository/ActeurRepository.java
@@ -5,6 +5,10 @@ import aberthier.projet.model.Acteur;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Set;
+
 @RepositoryRestResource(collectionResourceRel = "acteur", path = "acteurs")
 public interface ActeurRepository extends CrudRepository<Acteur, Integer> {
+    Set<Acteur> findActeursByNom(String nom);
+    Set<Acteur> findActeursByPrenom(String prenom);
 }
--- src/main/java/aberthier/projet/repository/FilmRepository.java
+++ src/main/java/aberthier/projet/repository/FilmRepository.java
@@ -5,6 +5,13 @@ import aberthier.projet.model.Film;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Set;
+
 @RepositoryRestResource(collectionResourceRel = "film", path = "films")
 public interface FilmRepository extends CrudRepository<Film, Integer> {
+    Set<Film> findFilmsByActeurNom(String nom);
+
+    Set<Film> findFilmsByGenre(String genre);
+
+    Set<Film> findFilmsByTitreContaining(String titre);
 }
